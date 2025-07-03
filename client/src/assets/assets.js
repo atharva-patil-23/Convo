bda83f9f7fe83ed49a1ce6e9549cd3147d7b013d
Atharva Patil <anpatil.1223@gmail.com>
2025-07-03 10:55:01 +0530

    add new assets and update ChatContainer to utilize images in messages

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,38 +1,81 @@
+import avatar_icon from './avatar.svg'
+import gallery_icon from './gallery_icon.svg'
+import help_icon from './info.svg'
+import logo_icon from './chat.svg'
+import logo_big from './chat.svg'
+import logo from './chat.svg'
+import profile_richard from './profile_richard.png'
+import profile_alison from './profile_alison.png'
+import profile_enrique from './profile_enrique.png'
+import profile_marco from './profile_marco.png'
+import profile_martin from './profile_martin.png'
+import search_icon from './search.svg'
+import send_button from './send_button.svg'
+import menu_icon from './menu-icon.svg'
+import arrow_icon from './arrow_icon.png'
+import code from './code.svg'
+import bgImage from './bg-image.jpg'
+import pic1 from './pic1.png'
+import pic2 from './pic2.png'
+import pic3 from './pic3.png'
+import pic4 from './pic4.png'
+import img1 from './img1.jpg'
+import img2 from './img2.jpg'
+
+const assets = {
+    avatar_icon,
+    gallery_icon,
+    help_icon,
+    logo_big,
+    logo_icon,
+    logo,
+    search_icon,
+    send_button,
+    menu_icon,
+    arrow_icon,
+    code,
+    bgImage,
+    profile_martin
+}
+
+export default assets;
+
+export const imagesDummyData = [pic1, pic2, pic3, pic4, pic1, pic2]
 
 export const userDummyData = [
     {
         "_id": "680f50aaf10f3cd28382ecf2",
         "email": "test1@greatstack.dev",
         "fullName": "Alison Martin",
-        "profilePic": "",
+        "profilePic": profile_alison,
         "bio": "Hi Everyone, I am Using QuickChat",
     },
     {
         "_id": "680f50e4f10f3cd28382ecf9",
         "email": "test2@greatstack.dev",
         "fullName": "Martin Johnson",
-        "profilePic": "",
+        "profilePic": profile_martin,
         "bio": "Hi Everyone, I am Using QuickChat",
     },
     {
         "_id": "680f510af10f3cd28382ed01",
         "email": "test3@greatstack.dev",
         "fullName": "Enrique Martinez",
-        "profilePic":"",
+        "profilePic": profile_enrique,
         "bio": "Hi Everyone, I am Using QuickChat",
     },
     {
         "_id": "680f5137f10f3cd28382ed10",
         "email": "test4@greatstack.dev",
         "fullName": "Marco Jones",
-        "profilePic": "",
+        "profilePic": profile_marco,
         "bio": "Hi Everyone, I am Using QuickChat",
     },
     {
         "_id": "680f516cf10f3cd28382ed11",
         "email": "test5@greatstack.dev",
         "fullName": "Richard Smith",
-        "profilePic": "",
+        "profilePic": profile_richard,
         "bio": "Hi Everyone, I am Using QuickChat",
     }
 ]
@@ -74,7 +117,7 @@ export const messagesDummyData = [
         "_id": "680f573cf10f3cd28382f0c0",
         "senderId": "680f50e4f10f3cd28382ecf9",
         "receiverId": "680f5116f10f3cd28382ed02",
-        "image": "",
+        "image": img1,
         "seen": true,
         "createdAt": "2025-04-28T10:23:56.265Z",
     },
@@ -82,7 +125,7 @@ export const messagesDummyData = [
         "_id": "680f5745f10f3cd28382f0c5",
         "senderId": "680f5116f10f3cd28382ed02",
         "receiverId": "680f50e4f10f3cd28382ecf9",
-        "image": "",
+        "image": img2,
         "seen": true,
         "createdAt": "2025-04-28T10:24:05.164Z",
     },
