a50e4599b406de75c2caf2c86e0cafcdd7c9bedc
Atharva Patil <anpatil.1223@gmail.com>
2025-07-07 03:39:48 +0530

    done with backend

--- server/routes/message.routes.js
+++ server/routes/message.routes.js
@@ -1,11 +1,12 @@
 import express from "express"
 import { protectRoute } from "../middlewares/auth.middleware.js"
-import { getMessages, getUserforSidebar, markMessageAsSeen } from "../controllers/message.controller.js"
+import { getMessages, getUserforSidebar, markMessageAsSeen, sendMessage } from "../controllers/message.controller.js"
 
 const messageRouter = express.Router()
 
 messageRouter.get("/users",protectRoute, getUserforSidebar)
 messageRouter.get("/:id",protectRoute, getMessages)
 messageRouter.put("mark/:id",protectRoute, markMessageAsSeen)
+messageRouter.post("/send/:id",protectRoute, sendMessage)
 
 export default messageRouter
