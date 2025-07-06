b2454fb7cfb03d788d1860933388901d520ec122
Atharva Patil <anpatil.1223@gmail.com>
2025-07-07 03:22:02 +0530

    add messaging functionality with message model, controller, and routes

---
+++ server/routes/message.routes.js
@@ -0,0 +1,11 @@
+import express from "express"
+import { protectRoute } from "../middlewares/auth.middleware.js"
+import { getMessages, getUserforSidebar, markMessageAsSeen } from "../controllers/message.controller.js"
+
+const messageRouter = express.Router()
+
+messageRouter.get("/users",protectRoute, getUserforSidebar)
+messageRouter.get("/:id",protectRoute, getMessages)
+messageRouter.put("mark/:id",protectRoute, markMessageAsSeen)
+
+export default messageRouter
--- server/server.js
+++ server/server.js
@@ -3,15 +3,37 @@ import "dotenv/config"
 import cors from "cors"
 import http from "http"
 import { connectDB } from "./lib/db.js"
+import userRouter from "./routes/user.routes.js"
+import messageRouter from "./routes/message.routes.js"
+import { Server } from "socket.io"
 
 const app = express()
 const server = http.createServer(app)
+
+export const io = new Server({
+    cors :{origin:"*"}
+})
+
+io.on("connection", (socket) =>{
+    const userId = socket.handshake.query.userId;
+    console.log("User connected" , userId)
+
+    if(userId) userSocketMap[userId] = socket.id;
+
+    io.emit("getOnlineUsers" , Object.keys(userSocketMap))
+})
+
+
 
 app.use(express.json({limit:"4mb"}))
 app.use(cors());
 
 
+
 app.use("/api/status",(req,res) => res.send("Server is live"));
+
+app.use("/api/auth", userRouter)
+app.use("/api/messages",messageRouter)
 
 await connectDB()
 
