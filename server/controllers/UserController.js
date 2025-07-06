b513ccbef5974465c2c4531cfb5b3d0103c3e2c9
Atharva Patil <anpatil.1223@gmail.com>
2025-07-06 23:10:00 +0530

    add profile update functionality with Cloudinary integration for image uploads

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js"
 import User from "../models/UserModel.js"
 import bcrypt from "bcryptjs"
+import cloudinary from "../lib/cloudinary.js"
 
 export const signup = async (req,res) => {
     const {fullName , email , password , bio} = req.body
@@ -90,4 +91,31 @@ export const checkAuth = (req,res) => {
         success:true,
         user:req.user
     })
+}
+
+
+export const updateProfile = async(req,res) => {
+    try {
+        const {profilePic, bio , fullName} =  req.body;
+        const userId = req.user._id
+        let updatedUser ;
+
+        if(!profilePic){
+            updatedUser = await User.findByIdAndUpdate(userId, {bio,fullName},{new:true})
+        }else{
+            const upload = await cloudinary.uploader.upload(profilePic)
+
+            updatedUser = await User.findByIdAndUpdate(userId , {profilePic:upload.secure_url, bio , fullName} , {new:true}) 
+        }
+        res.json({
+            success:true,
+            user:updatedUser
+        })
+    } catch (error) {
+        console.log(error.message)
+        res.json({
+            success:false,
+            user:error.message
+        })
+    }
 }
