fa09b1e12b79986766ee424bde86fcf6174d2910
Atharva Patil <anpatil.1223@gmail.com>
2025-07-07 20:07:30 +0530

    add authentication context with socket.io integration and toast notifications

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,13 @@ import React from "react"
 import HomePage from "./pages/HomePage.jsx"
 import LoginPage from "./pages/LoginPage.jsx"
 import ProfilePage from "./pages/ProfilePage.jsx"
+import {Toaster} from "react-hot-toast"
 
 function App() {
   return (
     <>
     <div className="bg-[url('./src/assets/bg-image.jpg')] bg-cover">
+      <Toaster />
       <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
