822346d42b033c7dca88e01f263d8e3d33da8c7d
Atharva Patil <anpatil.1223@gmail.com>
2025-07-07 20:21:58 +0530

    implement authentication-based routing for protected pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,22 @@
-import { Route, Routes } from "react-router-dom"
-import React from "react"
+import { Navigate, Route, Routes } from "react-router-dom"
+import React, { useContext } from "react"
 import HomePage from "./pages/HomePage.jsx"
 import LoginPage from "./pages/LoginPage.jsx"
 import ProfilePage from "./pages/ProfilePage.jsx"
 import {Toaster} from "react-hot-toast"
+import { AuthContext } from "../context/auth.context.jsx"
 
 function App() {
+
+  const { authUser } = useContext(AuthContext)
   return (
     <>
     <div className="bg-[url('./src/assets/bg-image.jpg')] bg-cover">
       <Toaster />
       <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/profile" element={<ProfilePage />} />   
+          <Route path="/" element={authUser? <HomePage /> :<Navigate to='/login' />} />
+          <Route path="/login" element={!authUser ?<LoginPage /> : <Navigate to='/' />} />
+          <Route path="/profile" element={authUser? <ProfilePage /> : <Navigate to='/login' />} />   
       </Routes>
     </div>
     </>
